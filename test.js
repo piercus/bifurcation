b354bae0eaf075117e8efd5d8c41cf75956d62e5
Pierre Colle <piercus@gmail.com>
2018-02-14 16:53:22 +0100

    bifurcation speed up

--- test.js
+++ test.js
@@ -34,7 +34,9 @@ const testChaos = [{
 			precision: 0.01
 		},
 		output: {
-			chaos: [[0.258990989547609, 0.9236766206909823]]
+			chaos: [
+				[0.258990989547609, 0.9236766206909823]
+			]
 		}
 	},{
 		input : {
@@ -49,50 +51,88 @@ const testChaos = [{
 	},{
 		input : {
 			param: 3.7,
-			precision: 0.0001,
+			precision: 0.000001,
 			min: 0.3999,
 			max: 0.4
 		},
 		output: {
 			chaos: [[0.3999, 0.4]]
 		}
+	},{
+		input : {
+			param: 3.7,
+			precision: 0.0000001,
+			min: 0.399999,
+			max: 0.4
+		},
+		output: {
+			chaos: [[0.399999, 0.4]]
+		}
+	},{
+		input : {
+			param: 3.7,
+			precision: 0.00000001,
+			min: 0.399999,
+			max: 0.4
+		},
+		output: {
+			chaos: [[0.3999999, 0.4]]
+		}
 }];
 
-const macro = (t, {fn, input, expected, label}) => {
-	const bif = new Bif({fn});
-	const startDate = new Date();
+const macro = (t, {fn, inverse, input, expected, label}) => {
+	const bif = new Bif({
+		fn,
+		inverse: inverse
+	});
+	console.time(label)
 	return bif.attractors(input).then(v => {
 		t.is(typeof(v.attractors), typeof(expected.attractors), v.attractors+' vs '+expected.attractors)
 		if(v.attractors){
 			for(var i = 0; i < v.attractors.length; i++){
-				t.true(Math.abs(v.attractors[i] - expected.attractors[i]) < (input.precision || 0.0001), v.attractors[i] + ' is different than ' + expected.attractors[i]);
+				const precision = (input.precision || 0.0001)*2;
+				t.true(Math.abs(v.attractors[i] - expected.attractors[i]) < precision, v.attractors[i] + ' is different than ' + expected.attractors[i] + ' for precision ' + precision);
 			}
 		}
 		if(v.chaos){
 			for(var i = 0; i < v.chaos.length; i++){
-				t.true(Math.abs(v.chaos[i][0] - expected.chaos[i][0]) < (input.precision || 0.0001), v.chaos[i][0] + ' is different than ' + expected.chaos[i][0]);
-				t.true(Math.abs(v.chaos[i][1] - expected.chaos[i][1]) < (input.precision || 0.0001), v.chaos[i][1] + ' is different than ' + expected.chaos[i][1]);
+				//console.log(v.chaos[i], v.chaos, input);
+				const precision = (input.precision || 0.0001)*2;
+				const rangeStart = v.chaos[i][0];
+				const expectedStart =  expected.chaos[i][0];
+				const rangeEnd = v.chaos[i][1];
+				const expectedEnd =  expected.chaos[i][1];				
+				t.true(Math.abs(rangeStart - expectedStart) < precision, rangeStart + ' is different than ' + expectedStart + ' for precision ' + precision);
+				t.true(Math.abs(rangeEnd - expectedEnd) < precision, rangeEnd + ' is different than ' + expectedEnd + ' for precision ' + precision);
 			}
 		}
-		const endDate = new Date();
-		console.log(label + ' : ' + ((endDate.getTime() - startDate.getTime())/1000) + ' sec');
+		console.timeEnd(label)
 	})
 }
 
-/*for (var i = 0; i< testsAttractors.length ; i++){
+for (var i = 0; i< testsAttractors.length ; i++){
 	const input = testsAttractors[i].input;
 	const expected = testsAttractors[i].output;
 	const fn = (a,x) => {return a*x*(1-x)}
 	const label = 'simple attractors with logistic a.x(1-x) ' + i;
 	test.serial(label, macro, {fn, input, expected, label});
-}*/
+}
 
 
 for (var i = 0; i< testChaos.length ; i++){
 	const input = testChaos[i].input;
 	const expected = testChaos[i].output;
 	const label = 'chaos with logistic a.x(1-x) ' + i;
-	const fn = (a,x) => {return a*x*(1-x)}
+	const fn = (a,x) => {return a*x*(1-x)};
+	const inverse = (a,x) => {
+		let delta = 1-4*x/a;
+		if(delta < 0){
+			return [];
+		} else {
+			const d = Math.sqrt(delta);
+			return [(1-d)/2, (1+d)/2];
+		}
+	}
 
 	test.serial(label, macro, {fn, input, expected, label});
 }
